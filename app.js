f22052d1eb8947cb6e58f11e28d3e08f962247c8
KZ <kjreem.imagine@gmail.com>
2022-12-30 01:28:25 +0330

    solved CORS error but will need refactoring

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const DATABASE = require('./database/connect')
-const path = require('path')
-const PORT = process.env.PORT || 3000
-const ACCOUNT = require('./api/accountRouter.js')
-const USER = require('./api/userRouter.js')
-const IMAGE = require('./api/imageRouter.js')
-const CONTRACT = require('./api/contractRouter.js')
-const ADMIN = require('./api/adminRouter.js')
-const PAYMENTHISTORY = require('./api/paymentHistoryRouter.js')
+const DATABASE = require("./database/connect");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const ACCOUNT = require("./api/accountRouter.js");
+const USER = require("./api/userRouter.js");
+const IMAGE = require("./api/imageRouter.js");
+const CONTRACT = require("./api/contractRouter.js");
+const ADMIN = require("./api/adminRouter.js");
+const PAYMENTHISTORY = require("./api/paymentHistoryRouter.js");
+const cors = require("cors");
 
-
-app.use(express.json()) // middleware
-app.use('/', express.static(path.resolve(__dirname, '..', '..', 'production/')))
+app.use(cors());
+app.use(express.json()); // middleware
+app.use(
+  "/",
+  express.static(path.resolve(__dirname, "..", "..", "production/"))
+);
 
 // api routes
-app.use("/api/paymenthistories", PAYMENTHISTORY)
-app.use("/api/contracts", CONTRACT)
-app.use("/api/accounts", ACCOUNT)
-app.use("/api/admins", ADMIN)
-app.use("/api/users", USER)
+app.use("/api/paymenthistories", PAYMENTHISTORY);
+app.use("/api/contracts", CONTRACT);
+app.use("/api/accounts", ACCOUNT);
+app.use("/api/admins", ADMIN);
+app.use("/api/users", USER);
 
 DATABASE.connect()
-    .then(() => console.log("You have been successfully connected to the Database!"))
-    .then(() => app.listen(PORT, () => {
-        console.log(`Express Server has started on port ${PORT}! See http://localhost:${PORT}/`)
-    }));
+  .then(() =>
+    console.log("You have been successfully connected to the Database!")
+  )
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(
+        `Express Server has started on port ${PORT}! See http://localhost:${PORT}/`
+      );
+    })
+  );
 
 module.exports = app;
